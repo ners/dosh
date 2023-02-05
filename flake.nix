ab34c37f633443a6504603a40d934a6de361469e
ners <ners@gmx.ch>
2023-02-05 15:30:28 +0100

    add flake-compat and vscode settings

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem
