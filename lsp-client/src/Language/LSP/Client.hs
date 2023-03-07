3543d1c30c4621176878de48832fa891cf544ed4
ners <ners@gmx.ch>
2023-03-07 17:42:35 +0100

    express session with STM and ReaderT

--- lsp-client/src/Language/LSP/Client.hs
+++ lsp-client/src/Language/LSP/Client.hs
@@ -4,41 +4,53 @@ module Language.LSP.Client where
 
 import Control.Lens
 import Control.Monad (forever)
+import Control.Monad.Reader (ReaderT, runReaderT)
 import Data.Aeson (decode)
 import Data.ByteString (fromStrict, hGetLine)
 import Data.Either (fromRight)
 import Data.Maybe (fromJust)
 import Language.LSP.Types
-import System.IO (Handle)
-import UnliftIO (race)
+import System.IO (Handle, stdin, stdout)
+import UnliftIO (liftIO, race)
+import UnliftIO.STM
 import Prelude
 
--- data SessionState = SessionState
---    { sendingQueue :: [Message] -- Probably not needed
---    , requestsWaitingForResponse :: Map (LspId, LspRequest) (LspRequest -> LspResponse -> IO ())
---    , lastDiagnostics :: Maybe [Diagnostics]
---    }
---
+data SessionState = SessionState
+    { sendingQueue :: ()
+    , -- [Message] -- Probably not needed
+      requestsWaitingForResponse :: ()
+    , -- Map (LspId, LspRequest) (LspRequest -> LspResponse -> IO ())
+      lastDiagnostics :: Maybe [Diagnostic]
+    }
+
+defaultSessionState :: SessionState
+defaultSessionState =
+    SessionState
+        { sendingQueue = ()
+        , requestsWaitingForResponse = ()
+        , lastDiagnostics = Nothing
+        }
+
 class HasDiagnostics a where
     diagnostics :: Lens' a (Maybe [Diagnostic])
 
---
-type Session = IO
+type Session = ReaderT (TVar SessionState) IO
 
 data ServerResponse
 
---
--- runSession :: Session a -> IO a
--- runSession = runSessionWithHandles stdin stdout
+runSession :: Session a -> IO a
+runSession = runSessionWithHandles stdin stdout
 
 runSessionWithHandles :: Handle -> Handle -> Session a -> IO a
 runSessionWithHandles input output action = do
-    actionResult <- race action $ forever $ do
-        serverMessage <- fromJust . decode . fromStrict <$> hGetLine input
-        if isDiagnostics serverMessage
-            then pure () -- update state to save lastDiagnostics
-            else pure ()
-    pure $ fromRight undefined actionResult
+    initialState <- newTVarIO defaultSessionState
+    flip runReaderT initialState $ do
+        actionResult <- race action $ forever $ do
+            serverMessage <- fromJust . decode . fromStrict <$> liftIO (hGetLine input)
+            if isDiagnostics serverMessage
+                then pure () -- update state to save lastDiagnostics
+                else pure ()
+        pure $ fromRight undefined actionResult
 
 sendRequest
     :: SClientMethod m
--- lsp-client/test/Language/LSP/ClientSpec.hs
+++ lsp-client/test/Language/LSP/ClientSpec.hs
@@ -1,32 +1,92 @@
 module Language.LSP.ClientSpec where
 
+import Colog.Core (LogAction (..), Severity (..), WithSeverity (..), (<&))
+import Colog.Core qualified as Colog
+import Control.Arrow ((>>>))
+import Control.Exception
 import Control.Monad
+import Data.Aeson (FromJSON, ToJSON, fromJSON)
+import Data.Aeson qualified as Aeson
+import Data.ByteString.Lazy (LazyByteString)
+import Data.ByteString.Lazy.Char8 qualified as LazyByteString
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.Prettyprint.Doc
+import GHC.Generics (Generic)
 import Language.LSP.Client
+import Language.LSP.Logging (defaultClientLogger)
+import Language.LSP.Server (Handlers, LspM, ServerDefinition (..), defaultOptions, runServerWithHandles)
 import Language.LSP.Types
 import System.IO
 import System.Process (createPipe)
 import Test.Hspec
-import Prelude
-import Control.Arrow ((>>>))
-import UnliftIO (race, MonadUnliftIO, fromEither)
+import UnliftIO (MonadIO (..), MonadUnliftIO, fromEither, race)
 import UnliftIO.Concurrent
-import Control.Exception
+import UnliftIO.STM
+import Prelude hiding (log)
 
 withTimeout :: forall m a. MonadUnliftIO m => Int -> m a -> m a
 withTimeout delay a = fromEither =<< race timeout a
-    where
-        timeout = do
-            threadDelay delay
-            pure $ AssertionFailed "Timeout exceeded"
+  where
+    timeout = do
+        threadDelay delay
+        pure $ AssertionFailed "Timeout exceeded"
+
+addHeader :: LazyByteString -> LazyByteString
+addHeader content =
+    mconcat
+        [ "Content-Length: "
+        , LazyByteString.pack $ show $ LazyByteString.length content
+        , "\r\n"
+        , "\r\n"
+        , content
+        ]
+
+encode :: ToJSON a => a -> LazyByteString
+encode = addHeader . Aeson.encode
+
+data Config = Config {fooTheBar :: Bool, wibbleFactor :: Int}
+    deriving stock (Generic, Show)
+    deriving anyclass (ToJSON, FromJSON)
 
 -- | LSP server that does not read input, and sends dummy diagnostics once per second
 diagServer :: IO (Handle, Handle)
 diagServer = do
-    (_, inWrite) <- createPipe
+    (inRead, inWrite) <- createPipe
     (outRead, outWrite) <- createPipe
-    forkIO $ forM_ [1 ..] $ \i -> do
-        threadDelay 1_000_000
-        hPutStrLn outWrite $ "server diagnostics message " <> show i
+    let
+        -- Three loggers:
+        -- 1. To stderr
+        -- 2. To the client (filtered by severity)
+        -- 3. To both
+        stderrLogger :: LogAction IO (WithSeverity Text)
+        stderrLogger = Colog.cmap show Colog.logStringStderr
+        clientLogger :: LogAction (LspM Config) (WithSeverity Text)
+        clientLogger = defaultClientLogger
+        dualLogger :: LogAction (LspM Config) (WithSeverity Text)
+        dualLogger = clientLogger <> Colog.hoistLogAction liftIO stderrLogger
+        logToText = Text.pack . show . pretty
+
+        serverDefinition =
+            ServerDefinition
+                { defaultConfig = Config{fooTheBar = False, wibbleFactor = 0}
+                , onConfigurationChange = \_old v -> do
+                    case fromJSON v of
+                        Aeson.Error e -> Left (Text.pack e)
+                        Aeson.Success cfg -> Right cfg
+                , doInitialize = undefined
+                , staticHandlers = mempty
+                , interpretHandler = undefined
+                , options = defaultOptions
+                }
+    forkIO $
+        void $
+            runServerWithHandles
+                (Colog.cmap (fmap logToText) stderrLogger)
+                (Colog.cmap (fmap logToText) dualLogger)
+                inRead
+                outWrite
+                serverDefinition
     pure (inWrite, outRead)
 
 -- | LSP server that accepts requests and answers them with a delay
@@ -36,7 +96,7 @@ reqServer = do
     (outRead, outWrite) <- createPipe
     forkIO $ forever $ do
         req <- hGetLine inRead
-        let response = "req id ..."
+        let message = ResponseMessage "2.0" (Just $ IdInt 0) (Right Empty)
         forkIO $ threadDelay 100_000 >> hPutStrLn outWrite response
     pure (inWrite, outRead)
 
@@ -57,25 +117,35 @@ spec = do
         threadDelay 1_000_000
         input $ do
             d <- withTimeout 1_000_000 getDiagnostics
-            d `shouldBe` [] --server diagnostics message n
+            d `shouldBe` [] -- server diagnostics message n
         threadDelay 1_000_000
         input $ do
             d <- withTimeout 1_000_000 getDiagnostics
-            d `shouldBe` [] --server diagnostics message >n
+            d `shouldBe` [] -- server diagnostics message >n
     it "answers requests correctly" $ do
         input <- reqServer >>= uncurry client
         req1Done <- newEmptyMVar
         req1Id <- newEmptyMVar
         input $ do
-            sendRequest SShutdown Empty (\reqId _ -> do
-                (reqId `shouldBe`) =<< takeMVar req1Id
-                putMVar req1Done ()) >>= putMVar req1Id
+            sendRequest
+                SShutdown
+                Empty
+                ( \reqId _ -> do
+                    (reqId `shouldBe`) =<< takeMVar req1Id
+                    putMVar req1Done ()
+                )
+                >>= putMVar req1Id
         req2Done <- newEmptyMVar
         req2Id <- newEmptyMVar
         input $ do
-            sendRequest SShutdown Empty (\reqId _ -> do
-                (reqId `shouldBe`) =<< takeMVar req2Id
-                putMVar req2Done ()) >>= putMVar req2Id
+            sendRequest
+                SShutdown
+                Empty
+                ( \reqId _ -> do
+                    (reqId `shouldBe`) =<< takeMVar req2Id
+                    putMVar req2Done ()
+                )
+                >>= putMVar req2Id
         tryTakeMVar req1Done `shouldReturn` Nothing
         tryTakeMVar req2Done `shouldReturn` Nothing
         threadDelay 1_100_000
