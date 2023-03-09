9d25850c609c9138f1a5812cce48d6678da128f1
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2023-03-09 17:08:00 +0100

    copy document-related functions from Language.LSP.Test
    `createDoc` has been commented out, because it uses a convoluted process
    to determine whether or not to send a notification to the server, using
    several variables that are stored in the session state.

--- lsp-client/src/Language/LSP/Client.hs
+++ lsp-client/src/Language/LSP/Client.hs
@@ -1,11 +1,23 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Language.LSP.Client where
 
 import Control.Concurrent.Async.Lifted ( concurrently_, race )
 import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
 import Control.Concurrent.STM
+-- import Data.Map.Strict qualified as Map
+
+-- import Data.Tree.Lens (root)
+
+-- import System.Directory (canonicalizePath)
+
+-- import System.FilePath.Glob qualified as Glob
+
+import Control.Exception (throw)
+import Control.Lens hiding (List)
 import Control.Monad (forever)
 import Control.Monad.IO.Class ( MonadIO(liftIO) )
 import Control.Monad.Reader (ReaderT, asks, runReaderT)
@@ -14,21 +26,29 @@ import Data.ByteString.Lazy (LazyByteString)
 import Data.ByteString.Lazy qualified as LazyByteString
 import Data.Coerce (coerce)
 import Data.Either (fromLeft)
-import Data.Functor (void, (<&>))
+import Data.Functor (void)
 import Data.Generics.Labels ()
 import Data.IxMap (insertIxMap)
-import Data.Maybe (fromMaybe, fromJust)
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
 import Language.LSP.Client.Compat ( getCurrentProcessID )
 import Language.LSP.Client.Decoding
 import Language.LSP.Client.Encoding (encode)
+import Language.LSP.Client.Exceptions (SessionException (UnexpectedResponseError))
 import Language.LSP.Types
-import Language.LSP.VFS (VFS, initVFS)
+import Language.LSP.Types.Lens hiding (applyEdit, capabilities, executeCommand, id, message, rename, to)
+import Language.LSP.VFS
+    ( VFS
+    , initVFS
+    , vfsMap
+    , virtualFileText
+    , virtualFileVersion
+    )
+import System.FilePath ((</>))
 import System.IO (Handle, stdin, stdout)
 import Prelude
 import Language.LSP.Types.Capabilities (fullCaps)
-import Language.LSP.Client.Exceptions
-    ( SessionException(UnexpectedResponseError) )
-import Control.Exception (throw)
 
 data SessionState = SessionState
     { initialized :: TMVar InitializeResult
@@ -38,9 +58,10 @@ data SessionState = SessionState
     , incoming :: TQueue LazyByteString
     -- ^ bytes that have been read from the input handle, but not yet parsed
     , outgoing :: TQueue LazyByteString
-    -- ^ messages that have been serialised but not yet written to the output handle
+    -- ^ messages that have been serialized but not yet written to the output handle
     , vfs :: TVar VFS
     -- ^ virtual, in-memory file system of the files known to the LSP
+    , rootDir :: FilePath
     }
 
 defaultSessionState :: VFS -> IO SessionState
@@ -52,6 +73,7 @@ defaultSessionState vfs' = do
     outgoing <- newTQueueIO
     incoming <- newTQueueIO
     vfs <- newTVarIO vfs'
+    rootDir <- undefined
     pure SessionState{..}
 
 type Session = ReaderT SessionState IO
@@ -178,3 +200,129 @@ getCompletions doc pos = do
   case getResponseResult response of
     InL (List items) -> pure items
     InR (CompletionList{_items = List items}) -> pure items
+
+{- | /Creates/ a new text document. This is different from 'openDoc'
+ as it sends a workspace/didChangeWatchedFiles notification letting the server
+ know that a file was created within the workspace, __provided that the server
+ has registered for it__, and the file matches any patterns the server
+ registered for.
+ It /does not/ actually create a file on disk, but is useful for convincing
+ the server that one does exist.
+-}
+
+-- createDoc
+--     :: FilePath
+--     -- ^ The path to the document to open, __relative to the root directory__.
+--     -> T.Text
+--     -- ^ The text document's language identifier, e.g. @"haskell"@.
+--     -> T.Text
+--     -- ^ The content of the text document to create.
+--     -> Session TextDocumentIdentifier
+--     -- ^ The identifier of the document just created.
+-- createDoc file languageId contents = do
+--     dynCaps <- curDynCaps <$> get
+--     rootDir <- asks rootDir
+--     caps <- asks sessionCapabilities
+--     absFile <- liftIO $ canonicalizePath (rootDir </> file)
+--     let pred :: SomeRegistration -> [Registration WorkspaceDidChangeWatchedFiles]
+--         pred (SomeRegistration r@(Registration _ SWorkspaceDidChangeWatchedFiles _)) = [r]
+--         pred _ = mempty
+--         regs = concatMap pred $ Map.elems dynCaps
+--         watchHits :: FileSystemWatcher -> Bool
+--         watchHits (FileSystemWatcher pattern kind) =
+--             -- If WatchKind is excluded, defaults to all true as per spec
+--             fileMatches (T.unpack pattern) && createHits (fromMaybe (WatchKind True True True) kind)
+
+--         fileMatches pattern = Glob.match (Glob.compile pattern) relOrAbs
+--           where
+--             -- If the pattern is absolute then match against the absolute fp
+--             relOrAbs
+--                 | isAbsolute pattern = absFile
+--                 | otherwise = file
+
+--         createHits (WatchKind create _ _) = create
+
+--         regHits :: Registration WorkspaceDidChangeWatchedFiles -> Bool
+--         regHits reg = foldl' (\acc w -> acc || watchHits w) False (reg ^. registerOptions . watchers)
+
+--         clientCapsSupports =
+--             caps
+--                 ^? workspace
+--                 . _Just
+--                 . didChangeWatchedFiles
+--                 . _Just
+--                 . dynamicRegistration
+--                 . _Just
+--                 == Just True
+--         shouldSend = clientCapsSupports && foldl' (\acc r -> acc || regHits r) False regs
+
+--     when shouldSend $
+--         sendNotification SWorkspaceDidChangeWatchedFiles $
+--             DidChangeWatchedFilesParams $
+--                 List [FileEvent (filePathToUri (rootDir </> file)) FcCreated]
+--     openDoc' file languageId contents
+
+{- | Opens a text document that /exists on disk/, and sends a
+ textDocument/didOpen notification to the server.
+-}
+openDoc :: FilePath -> T.Text -> Session TextDocumentIdentifier
+openDoc file languageId = do
+    rootDir <- asks rootDir
+    let fp = rootDir </> file
+    contents <- liftIO $ T.readFile fp
+    openDoc' file languageId contents
+
+{- | This is a variant of `openDoc` that takes the file content as an argument.
+ Use this is the file exists /outside/ of the current workspace.
+-}
+openDoc' :: FilePath -> T.Text -> T.Text -> Session TextDocumentIdentifier
+openDoc' file languageId contents = do
+    rootDir <- asks rootDir
+    let fp = rootDir </> file
+        uri = filePathToUri fp
+        item = TextDocumentItem uri languageId 0 contents
+    sendNotification STextDocumentDidOpen (DidOpenTextDocumentParams item)
+    pure $ TextDocumentIdentifier uri
+
+-- | Closes a text document and sends a textDocument/didOpen notification to the server.
+closeDoc :: TextDocumentIdentifier -> Session ()
+closeDoc docId = do
+    let params = DidCloseTextDocumentParams (TextDocumentIdentifier (docId ^. uri))
+    sendNotification STextDocumentDidClose params
+
+-- | Changes a text document and sends a textDocument/didOpen notification to the server.
+changeDoc :: TextDocumentIdentifier -> [TextDocumentContentChangeEvent] -> Session ()
+changeDoc docId changes = do
+    verDoc <- getVersionedDoc docId
+    let params = DidChangeTextDocumentParams (verDoc & version . non 0 +~ 1) (List changes)
+    sendNotification STextDocumentDidChange params
+
+-- | Gets the Uri for the file corrected to the session directory.
+getDocUri :: FilePath -> Session Uri
+getDocUri file = do
+    rootDir <- asks rootDir
+    let fp = rootDir </> file
+    return $ filePathToUri fp
+
+-- | Returns the symbols in a document.
+getDocumentSymbols :: TextDocumentIdentifier -> Session (Either [DocumentSymbol] [SymbolInformation])
+getDocumentSymbols doc = do
+    ResponseMessage _ rspLid res <- request STextDocumentDocumentSymbol (DocumentSymbolParams Nothing Nothing doc)
+    case res of
+        Right (InL (List xs)) -> return (Left xs)
+        Right (InR (List xs)) -> return (Right xs)
+        Left err -> throw (UnexpectedResponseError (SomeLspId $ fromJust rspLid) err)
+
+-- | The current text contents of a document.
+documentContents :: TextDocumentIdentifier -> Session T.Text
+documentContents doc = do
+    vfs <- asks vfs >>= liftIO . readTVarIO
+    let Just file = vfs ^. vfsMap . at (toNormalizedUri (doc ^. uri))
+    return (virtualFileText file)
+
+-- | Adds the current version to the document, as tracked by the session.
+getVersionedDoc :: TextDocumentIdentifier -> Session VersionedTextDocumentIdentifier
+getVersionedDoc (TextDocumentIdentifier uri) = do
+    vfs <- asks vfs >>= liftIO . readTVarIO
+    let ver = vfs ^? vfsMap . ix (toNormalizedUri uri) . to virtualFileVersion
+    return (VersionedTextDocumentIdentifier uri ver)
